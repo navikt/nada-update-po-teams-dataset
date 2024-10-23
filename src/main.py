96f601276d4d215ae774843c1449211f15c05f3e
vebjorre <vebjorn.rekkebo@nav.no>
2024-10-23 14:00:28 +0200

    configure logger

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 import datetime
 import json
+import logging
 import os
 
 from google.cloud.bigquery import Client
@@ -7,25 +8,24 @@ from google.oauth2.service_account import Credentials
 
 from bq import df_to_table
 from config import TARGET_TABLE
-from logger import create_logger
 from markedsplassen import get_dp, get_ds
 from pandas_stuff import assign_unassigned, merge_sort_count
 from teamkatalogen import get_po, get_teams, link_naisteam_to_tk
 
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+
+
 if __name__ == '__main__':
-
-    _logger = create_logger(name="nada-data-analyse")
-
     credentials = Credentials.from_service_account_info(json.loads(os.environ["GOOGLE_SA_CREDS"]))
     client = Client(credentials=credentials, project=credentials.project_id)
 
-    _logger.info("Getting teams from teamkatalogen")
+    logging.info("Getting teams from teamkatalogen")
     df_team = get_teams()
-    _logger.info(f"Got {df_team.shape[0]} teams")
+    logging.info(f"Got {df_team.shape[0]} teams")
 
-    _logger.info("Getting product areas from teamkatalogen")
+    logging.info("Getting product areas from teamkatalogen")
     df_po = get_po()
-    _logger.info(f"Got {df_po.shape[0]} product areas")
+    logging.info(f"Got {df_po.shape[0]} product areas")
 
     # merge team og po
     df_all = df_team.merge(df_po, left_on="productAreaId", right_on="id", suffixes=["_team", "_po"])
@@ -42,14 +42,14 @@ if __name__ == '__main__':
 
     df_merged = merge_sort_count(df_dp, df_ds, df_tk)
 
-    df_merged = assign_unassigned(_logger, df_dp, df_ds, df_merged)
+    df_merged = assign_unassigned(logging, df_dp, df_ds, df_merged)
 
     df_merged["last_updated"] = datetime.datetime.now(datetime.UTC).isoformat()
 
-    _logger.info(f"Trying to overwrite table {TARGET_TABLE} with {df_merged.shape[0]} rows")
+    logging.info(f"Trying to overwrite table {TARGET_TABLE} with {df_merged.shape[0]} rows")
 
     try:
         err = df_to_table(client, df_merged, TARGET_TABLE)
-        _logger.info(f"Successfully updated table {TARGET_TABLE}")
+        logging.info(f"Successfully updated table {TARGET_TABLE}")
     except:
-        _logger.error("Could not update dataset")
+        logging.error("Could not update dataset")
